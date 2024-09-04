2a2d1965781f86b4ef54b843688657e8885f2407
maytepioli <maytepioli@gmail.com>
2024-09-04 11:50:21 -0300

    will assign a signed random number to the variable

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ elif number > 0:
     mod = number % 10
 if mod < 6 and mod != 0:
     print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
-elif mod > 5 and mod != 0:
-    print(f"Last digit of {number} is {mod} and is greater than 5 and not 0")
-elif mod == 0:
+elif mod > 5:
+    print(f"Last digit of {number} is {mod} and is greater than 5")
+else:
     print(f"Last digit of {number} is {mod}  and is 0")
