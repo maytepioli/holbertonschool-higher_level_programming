ae02ee6af369db12cbde7b470ce5771067fe1c92
mayte pioli <maytepioli@gmail.com>
2024-09-05 01:57:46 +0000

    print an uppercase string followed by a new line.

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def islower(c):
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    i = 0
+    while i < len(str):
+        if islower(str[i]):
+            ascii = ord(str[i])
+            ascii = ascii - 32
+            ascii = chr(ascii)
+        else:
+            ascii = str[i]
+        print("{}".format(ascii), end="")
+        i = i + 1
+    print()
