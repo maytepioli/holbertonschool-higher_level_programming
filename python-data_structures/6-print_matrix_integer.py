66b9a0e3a0f5ef9bebca3721a276530436420377
maytepioli <maytepioli@gmail.com>
2024-09-09 17:35:11 -0300

    prints an array of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        for element  in line:
-            print("{:d}".format(element), end=" ")
+        for element in line:
+            if element == line[-1]:
+                print("{:d}".format(element), end="")
+            else:
+                print("{:d}".format(element), end=" ")
         print()
