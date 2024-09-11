495e0f101c786bb5476687b73194f07acf44fc88
maytepioli <maytepioli@gmail.com>
2024-09-11 10:44:30 -0300

    unción que encuentre el número entero más grande de una lista

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    numax = my_list[0]
-    for i in range(1, len(my_list)):
-        if numax < my_list[i]:
-            numax = my_list[i]
-    return numax
+    for j in my_list:
+        numax = my_list[0]
+        for i in range(1, len(my_list)):
+            if numax < my_list[i]:
+                numax = my_list[i]
+        return numax
