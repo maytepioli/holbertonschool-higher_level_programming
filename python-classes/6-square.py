b90d1eb3336239750f8f171956d6bab70884e13c
maytepioli <maytepioli@gmail.com>
2024-09-18 09:53:47 -0300

    class Square that defines a square by: (based on 5-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -9,10 +9,6 @@ class Square:
     the class represents a square
     """
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
@@ -24,14 +20,13 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-
         for i in value:
             if not isinstance(i, int) or i < 0:
                 raise ValueError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     @property
-    def size(self)
+    def size(self):
         return self.__size
 
     @size.setter
