605a78da30c74337661984db2d310d2c4ec800b5
maytepioli <maytepioli@gmail.com>
2024-09-23 00:47:09 +0000

    that returns a new Rectangle instance with width == height == size

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         return f'Rectangle({self.__width}, {self.__height})'
 
     def __del__(self):
-        print("Bye rectangle...")
+        print("Bye rectangle..")
         Rectangle.number_of_instances -= 1
 
     def bigger_or_equal(rect_1, rect_2):
