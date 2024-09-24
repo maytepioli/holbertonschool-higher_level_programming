e9955f7abde53f4a2650775395d0d871892d5462
maytepioli <maytepioli@gmail.com>
2024-09-24 14:36:59 -0300

    empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+módulo base_geometry
+
+Define la clase BaseGeometry como interfaz base para geometrías.
+"""
+
+
+class BaseGeometry:
+    """
+    Clase BaseGeometry.
+
+    Esta clase sirve como una interfaz base para
+    otras clases de geometría.
+    No se debe instanciar directamente
+    y debe ser subclases para definir
+    geometrías específicas.
+    """
+    pass
