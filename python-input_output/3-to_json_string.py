25f3313ae0d010a9fb1f0e515dd5c31b052b015c
maytepioli <maytepioli@gmail.com>
2024-09-30 13:55:11 -0300

    function that returns the JSON representation of an object (string)

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+"""
+# Función que convierte un objeto a su representación en formato JSON
+"""
+
+
+def to_json_string(my_obj):
+
+    """
+    Convierte un objeto a formato JSON y devuelve la cadena JSON.
+    """
+
+    return json.dumps(my_obj)
