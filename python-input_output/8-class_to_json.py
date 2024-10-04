f7828e0331d3e59965cfba4516103750dfb4c022
maytepioli <maytepioli@gmail.com>
2024-10-04 18:56:58 -0300

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Importa el módulo 'json' para trabajar con datos en formato JSON.
+"""
+import json
+
+"""
+función que devuelve la descripción del diccionario con una
+estructura de datos simple
+"""
+
+
+def class_to_json(obj):
+    """
+    función que devuelve la descripción del diccionario con una
+    estructura de datos simple
+    """
+
+    dic = {}
+    dic = obj
+    return dic
