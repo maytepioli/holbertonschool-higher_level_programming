68903100f70ec50985d642ca6f3cda9c99277a6a
maytepioli <maytepioli@gmail.com>
2024-10-10 13:18:10 -0300

    Consuming and processing data from an API using Python

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,25 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    answer = requests.get(url)
+
+    if answer.status_code == 200:
+        posts = answer.json()
+        for i in posts:
+            print(f'{i["title"]}')
+
+def fetch_and_save_posts():
+    list_dic = []
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    answer = requests.get(url)
+    if answer.status_code == 200:
+        posts = answer.json()
+        for i in posts:
+           dic = {'id': i['id'], 'title': i['title'], 'body': i['body']}
+           list_dic.append(dic)
+    with open('posts.csv', 'w') as file:
+        write = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+        write.writeheader()
+        write.writerows(list_dic)
