4be9e97902f9291624757859b9d1592bb87e67f2
maytepioli <maytepioli@gmail.com>
2024-10-11 11:27:46 -0300

    Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,7 +40,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-Type','text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b'Not Found')
 
 with http.server.HTTPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
