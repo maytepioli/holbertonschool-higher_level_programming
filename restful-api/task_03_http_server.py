9584a50ab949ae465cce31935da7c831e5db9a2b
maytepioli <maytepioli@gmail.com>
2024-10-11 11:22:46 -0300

    Develop a simple API using Python with the  module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,52 @@
+import http.server
+import json
+
+PORT = 8000
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == '/data':
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-Type','text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+
+        elif self.path == '/info':
+            dic = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.send_response(200)
+            self.send_header('Content-Type','text/plain')
+            self.end_headers()
+            self.wfile.write(json.dumps(dic).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type','text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found: The requested resource does not exist.")
+
+with http.server.HTTPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+    
+
+    
+
+    
