c1093a9e723147c942aa9cbacdf4497d14f53c9f
maytepioli <maytepioli@gmail.com>
2024-10-30 13:48:17 -0300

    script that creates the MySQL server user user_0d_1.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- crea el usuario del servidor MySQL. user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
