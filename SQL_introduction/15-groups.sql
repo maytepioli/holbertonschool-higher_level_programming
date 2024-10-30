8450f8f25537abf144d125b1532dd81198183ddc
maytepioli <maytepioli@gmail.com>
2024-10-30 10:04:21 -0300

    script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 --  enumera la cantidad de registros con la misma puntuación en la tabla
+
 SELECT score, COUNT(*) AS number FROM second_table
-GROUP BY score GROUP BY score
+GROUP BY score
 ORDER BY number DESC;
