3b13f60d215f5fe70710fd41f0a120a4a69c66be
maytepioli <maytepioli@gmail.com>
2024-10-31 12:44:52 -0300

    script that creates the database hbtn_0d_2 and the user user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Crea la base de datos
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- crea el usuario con la indientifacion 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* 'user_0d_2'@'localhost';
+-- Otorga los privilegios
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
