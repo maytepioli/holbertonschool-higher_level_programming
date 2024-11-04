6ef778fe08aa0a69a7762a0075e0bc87404c2703
maytepioli <maytepioli@gmail.com>
2024-11-04 19:08:00 -0300

    add create table states

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,12 @@
 -- crea la base de datos. hbtn_0d_usa y la tabla cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (id INT  AUTO_INCREMENT PRIMARY KEY NOT NULL,
 name VARCHAR(256) NOT NULL);
+
 CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 states_id INT NOT NULL,
-name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
+, FOREIGN KEY (state_id) REFERENCES states(id),
+name VARCHAR(256) NOT NULL);
