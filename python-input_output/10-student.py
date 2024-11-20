adfded6caff349e643caa734adda1b36fc7971f6
maytepioli <maytepioli@gmail.com>
2024-11-20 13:49:37 -0300

    Write a class Student that defines a student by: (based on 9-student.py)

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,20 +3,22 @@
 Este archivo contiene una clase Estudiantes
 """
 
+
 class Student:
     """
     Este archivo contiene una clase Estudiantes
     """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         # Si no se pasa attrs, devuelve todos los atributos
         if attrs is None:
             return self.__dict__
-        
+
         # Si attrs es una lista, filtrar los atributos
         if type(attrs) is list:
             result = {}  # Cambié el nombre de dict a result
